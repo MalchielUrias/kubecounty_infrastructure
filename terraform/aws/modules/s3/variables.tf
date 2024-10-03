da426821e620d449c3e64ae148e612719849a376
Malchiel Urias <malchiel.urias@TerraAdminsMBP.lan>
2024-10-03 22:10:12 +0100

    s3 vars

--- terraform/aws/modules/s3/variables.tf
+++ terraform/aws/modules/s3/variables.tf
@@ -1,1 +1,27 @@
-
+variable "block_public_acls" {
+  type = string
+}
+variable "block_public_policy" {
+  type = string
+}
+variable "ignore_public_acls" {
+  type = string
+}
+variable "restrict_public_buckets" {
+  type = string
+}
+variable "policy" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "tags" {
+  type = map(string)
+}
+variable "lifecycle_rule" {
+  type = list(string)
+}
+variable "versioning_enabled" {
+  type = bool
+}
