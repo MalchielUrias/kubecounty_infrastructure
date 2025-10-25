af9543e8d81dfa6a48296ff161a2d801486d0218
Malchiel Urias <malchiel.urias@Terra-Admins-MacBook-Pro.local>
2025-10-25 22:05:16 +0100

    cloudfront module

---
+++ terraform/aws/modules/cloudfront/main.tf
@@ -0,0 +1,70 @@
+# CloudFront Origin Access Control (OAC)
+resource "aws_cloudfront_origin_access_control" "site" {
+  name                              = var.cloudfront_oac_name
+  description                       = "OAC for Cloudfront site"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "site" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  aliases             = [var.domain_name, "www.${var.domain_name}"]
+  price_class         = var.price_class  
+
+  origin {
+    domain_name              = aws_s3_bucket_website_configuration.site.website_endpoint
+    origin_id                = "S3-${var.bucket_name}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${var.bucket_name}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  # Custom error response for SPA-like behavior
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  tags = var.tags
+}
---
+++ terraform/aws/modules/cloudfront/variables.tf
@@ -0,0 +1,24 @@
+variable "tags" {
+  type = map(string)
+}
+
+variable "acm_certificate_arn" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "price_class" {
+  type = string
+  default = "PriceClass_100"
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "cloudfront_oac_name" {
+  type = string
+}
