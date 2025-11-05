f1e2b2f8b3f0916e101e7b611702e2960adae99d
Malchiel Urias <malchiel.urias@Terra-Admins-MacBook-Pro.local>
2025-11-05 07:53:56 +0100

    iamrole

--- terraform/aws/modules/iam/output.tf
+++ terraform/aws/modules/iam/output.tf
@@ -1,9 +1,9 @@
 output "role_name" {
   description = "The name of the IAM role."
-  value       = module.iam.iam_role_name
+  value       = module.this.iam_role_name
 }
 
 output "role_arn" {
   description = "The ARN of the IAM role."
-  value       = module.iam.iam_role_arn
+  value       = module.this.iam_role_arn
 }
