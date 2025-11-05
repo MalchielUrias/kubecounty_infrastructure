d22bd8e85a90e67bcde929959f7d22fd251a201b
Malchiel Urias <malchiel.urias@Terra-Admins-MacBook-Pro.local>
2025-11-05 06:24:19 +0100

    crreate cognito and lambda modules

--- terraform/aws/modules/cognito/main.tf
+++ terraform/aws/modules/cognito/main.tf
@@ -54,7 +54,7 @@ resource "aws_cognito_user_pool_client" "main" {
 # Cognito User Pool Domain
 resource "aws_cognito_user_pool_domain" "main" {
   domain       = "${var.domain_name}"
-  user_pool_id = aws_cognito_user_pool.main.id
+  user_pool_id = aws_cognito_user_pool.pool.id
 }
 
 
